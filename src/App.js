8e66fc1dab866820206bfdb0a463bfbc171e3ead
Bentike <ajadiyusuf73@gmail.com>
2022-11-28 15:45:50 +0100

    added Navbar.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Navbar from './Components/Navbar';
 
 function App() {
   return (
     <div className="App">
          <h1>Scandi</h1>
+         <Navbar/>
     </div>
   );
 }
