4f6a72bd1c268aedd92013407423572a0a932760
Bentike <ajadiyusuf73@gmail.com>
2022-12-02 15:32:48 +0100

    added Cartpage.js and ProductDescription.js

---
+++ src/Components/CartPage.js
@@ -0,0 +1,13 @@
+import React, {Component} from "react";
+
+class CartPage extends Component{
+    render(){
+        return(
+            <div>
+                Welcome To Cart Page
+            </div>
+        );
+    }
+}
+
+export default CartPage;
---
+++ src/Components/ProductDescription.js
@@ -0,0 +1,13 @@
+import React, {Component} from "react";
+
+class ProductDescription extends Component{
+    render(){
+        return(
+            <div>
+                Welcome To Product Description Page
+            </div>
+        );
+    }
+}
+
+export default ProductDescription;
