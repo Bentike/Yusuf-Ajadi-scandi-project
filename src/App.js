85a76c633681024aea7b4027c9bd797118064156
Bentike <ajadiyusuf73@gmail.com>
2022-12-02 14:32:42 +0100

    added category.css

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import Category from './Components/Category';
 import Navbar from './Components/Navbar';
 
 function App() {
   return (
     <div className="App">
          <Navbar/>
+         <Category/>
     </div>
   );
 }
--- src/Components/Category.js
+++ src/Components/Category.js
@@ -1,9 +1,15 @@
 import React, {Component} from "react";
+import '../Styles/Category.css';
 
 class Category extends Component{
     render(){
         return(
-            <div>Product Description</div>
+            <div className="category">
+                <h2>Category name</h2>
+                <div>
+
+                </div>
+            </div>
         )
     }
 }
