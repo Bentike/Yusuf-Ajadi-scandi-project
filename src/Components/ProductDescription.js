7e934c28d0e0f20528ffee5ba52de2987c69efc6
Bentike <ajadiyusuf73@gmail.com>
2022-12-05 14:09:21 +0100

    added Navbar to product page

--- src/Components/ProductDescription.js
+++ src/Components/ProductDescription.js
@@ -1,9 +1,11 @@
 import React, {Component} from "react";
+import Navbar from "./Navbar";
 
 class ProductDescription extends Component{
     render(){
         return(
             <div>
+                <Navbar/>
                 Welcome To Product Description Page
             </div>
         );
