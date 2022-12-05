127c6d837f2a57e26024f2b7d7076076825b2f99
Bentike <ajadiyusuf73@gmail.com>
2022-12-05 14:20:33 +0100

    added ProductDescription.css file

--- src/Components/ProductDescription.js
+++ src/Components/ProductDescription.js
@@ -1,12 +1,26 @@
 import React, {Component} from "react";
 import Navbar from "./Navbar";
+import wool from '../Images/wool.png';
+import '../Styles/ProductDescription.css';
 
 class ProductDescription extends Component{
     render(){
         return(
             <div>
                 <Navbar/>
-                Welcome To Product Description Page
+                <div className="product-wrap">
+                     <div className="product-bar">
+                         <img className="product-bar-img" src={wool} alt='product'/>
+                         <img className="product-bar-img" src={wool} alt='product'/>
+                         <img className="product-bar-img" src={wool} alt='product'/>
+                     </div>
+                     <div className="main-product">
+                         <img src={wool} alt='product'/>
+                     </div>
+                </div>
+                <div>
+                    Details
+                </div>
             </div>
         );
     }
