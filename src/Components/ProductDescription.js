8ea6cf58b6e2baae40bd28118b19c9851148fcc3
Bentike <ajadiyusuf73@gmail.com>
2022-12-05 15:29:16 +0100

    styled product description page

--- src/Components/ProductDescription.js
+++ src/Components/ProductDescription.js
@@ -8,18 +8,46 @@ class ProductDescription extends Component{
         return(
             <div>
                 <Navbar/>
-                <div className="product-wrap">
-                     <div className="product-bar">
-                         <img className="product-bar-img" src={wool} alt='product'/>
-                         <img className="product-bar-img" src={wool} alt='product'/>
-                         <img className="product-bar-img" src={wool} alt='product'/>
-                     </div>
-                     <div className="main-product">
-                         <img src={wool} alt='product'/>
-                     </div>
-                </div>
-                <div>
-                    Details
+                <div className="product">
+                    <div className="product-wrap">
+                        <div className="product-bar">
+                            <img className="product-bar-img" src={wool} alt='product'/>
+                            <img className="product-bar-img" src={wool} alt='product'/>
+                            <img className="product-bar-img" src={wool} alt='product'/>
+                        </div>
+                        <div className="main-product">
+                            <img src={wool} alt='product'/>
+                        </div>
+                    </div>
+                    <div className="details">
+                        <div className="title">
+                            <h2>Apollo</h2>
+                            <h3>Running Short</h3>
+                        </div>
+                        <p>Size:</p>
+                        <div className="sizes">
+                           <p>XS</p>
+                           <p>S</p>
+                           <p>M</p>
+                           <p>L</p>
+                        </div>
+                        <p>Color:</p>
+                        <div className="colors">
+                            <div></div>
+                            <div></div>
+                            <div></div>
+                        </div>
+                        <h3>Price:</h3>
+                        <p>$50.00</p>
+                        <button>ADD TO CART</button>
+                        <div className="description">
+                            <p className="description-text">
+                                Find stunning women's cocktail dresses and party dresses. 
+                                Stand out in lace and metallic cocktail dresses and party
+                                dresses from all your favorite brands.
+                            </p>
+                        </div>
+                    </div>
                 </div>
             </div>
         );
