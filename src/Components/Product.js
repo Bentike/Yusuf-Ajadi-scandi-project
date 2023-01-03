5f410ae33718ad137e37d22235ab340a0c7a3190
Bentike <ajadiyusuf73@gmail.com>
2023-01-03 17:46:16 +0100

    displayed cart btn on products

--- src/Components/Product.js
+++ src/Components/Product.js
@@ -1,4 +1,5 @@
 import React, {Component} from "react";
+import cart from '../Images/cart.png';
 import '../Styles/Product.css';
 
 class Product extends Component{
@@ -7,9 +8,10 @@ class Product extends Component{
 
         return(
             <div className="product-wrap">
-                 <img className="product-img" src={photo} alt='product'/>
+                 <img className="product-img" src={photo} alt="product"/>
                  <p>{productName}</p>
                  <p>{productPrice}</p>
+                 <img className="cart" src={cart} alt="add to cart"/>
             </div>
         );
     }
