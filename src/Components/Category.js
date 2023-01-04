1c45d9911ec67795c1250cd7d8f03fdde245b3a4
Bentike <ajadiyusuf73@gmail.com>
2023-01-04 21:18:43 +0100

    removed ref from Category

--- src/Components/Category.js
+++ src/Components/Category.js
@@ -5,22 +5,13 @@ import '../Styles/Category.css';
 import wool from '../Images/wool.png';
 
 class Category extends Component{
-  constructor(props){
-    super(props);
-    this.cartOnOff = React.createRef();
-  }
     render(){
-       
-        const handleMouseEnter = () => {
-            console.log(this.cartOnOff.current.style)
-        }
-
         return(
             <div className="category">
                 <Navbar/>
                 <h2 className="header">Category name</h2>
                 <div className="product-container">
-                    <Product ref={this.cartOnOff} handleMouseEnter={handleMouseEnter} photo={wool} productName='wool hood' productPrice='$50.00'/>
+                    <Product photo={wool} productName='wool hood' productPrice='$50.00'/>
                     <Product photo={wool} productName='wool hood' productPrice='$50.00'/>
                     <Product photo={wool} productName='wool hood' productPrice='$50.00'/>
                     <Product photo={wool} productName='wool hood' productPrice='$50.00'/>
