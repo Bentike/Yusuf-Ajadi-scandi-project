778c7483c2ebd0ce685d1838325a4ada5c7d685d
Bentike <ajadiyusuf73@gmail.com>
2023-01-05 09:33:17 +0100

    displayed dummy products in Men

--- src/Components/Men.js
+++ src/Components/Men.js
@@ -1,13 +1,22 @@
 import React, {Component} from "react";
 import Navbar from "./Navbar";
 import Product from "./Product";
+import wool from '../Images/wool.png';
 
 class Men extends Component{
     render(){
         return(
             <div>
                 <Navbar/>
-                <h1>Men Category</h1>
+                <h2 className="header">Men Category</h2>
+                <div className="product-container">
+                    <Product photo={wool} productName='wool hood' productPrice='$50.00'/>
+                    <Product photo={wool} productName='wool hood' productPrice='$50.00'/>
+                    <Product photo={wool} productName='wool hood' productPrice='$50.00'/>
+                    <Product photo={wool} productName='wool hood' productPrice='$50.00'/>
+                    <Product photo={wool} productName='wool hood' productPrice='$50.00'/>
+                    <Product photo={wool} productName='wool hood' productPrice='$50.00'/>
+                </div>
             </div>
         );
     }
