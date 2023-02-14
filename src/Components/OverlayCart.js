8e7c0c3f1d92ba5d0727be4c57c7e2c23df6efd2
bentike <ajadiyusuf73@gmail.com>
2023-02-14 12:52:33 +0100

    added contents to overlayCart component

--- src/Components/OverlayCart.js
+++ src/Components/OverlayCart.js
@@ -1,13 +1,44 @@
 import React, { Component } from "react";
-
-class OverlayCart extends Component{
-    render(){
-        return(
+import photo from "../Images/wool.png"
+class OverlayCart extends Component {
+  render() {
+    return (
+      <div>
+        <h3>
+          My Bag,<span> 3 items</span>
+        </h3>
+        <div>
+          <div>
+            <p>Apollo Running Short</p>
+            <p>$50.00</p>
+            <p>size</p>
             <div>
-                 hello
+                <div>XS</div>
+                <div>S</div>
+                <div>M</div>
+                <div>L</div>
             </div>
-        )
-    }
+            <p>color</p>
+            <div>
+                <div></div>
+                <div></div>
+                <div></div>
+            </div>
+          </div>
+          <div>
+              <div>
+                 <button>+</button>
+                 <p>1</p>
+                 <button>-</button>
+              </div>
+              <div>
+                <img src={photo} alt="item"/>
+              </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
-export default OverlayCart
+export default OverlayCart;
