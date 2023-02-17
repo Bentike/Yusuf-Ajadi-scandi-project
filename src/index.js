841a6caf9f672a5008c289f8e3965504842c9daa
bentike <ajadiyusuf73@gmail.com>
2023-02-17 12:51:11 +0100

    initiallized appllo client

--- src/index.js
+++ src/index.js
@@ -5,6 +5,11 @@ import { ApolloClient, InMemoryCache, ApolloProvider, gql } from '@apollo/client
 import './index.css';
 import App from './App';
 
+const client = new ApolloClient({
+  uri: 'https://flyby-gateway.herokuapp.com/',
+  cache: new InMemoryCache(),
+});
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
