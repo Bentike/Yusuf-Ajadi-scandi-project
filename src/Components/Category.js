b1e5377c2504c64dc7524bca731c7c96683fe8cc
bentike <ajadiyusuf73@gmail.com>
2023-02-20 22:24:38 +0100

    refactored query

--- src/Components/Category.js
+++ src/Components/Category.js
@@ -6,11 +6,12 @@ import "../Styles/Category.css";
 import wool from "../Images/wool.png";
 
 class Category extends Component {
-  componentDidMount(){
-    fetch('http://localhost:4000', {
-        method: 'GET',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ query: `
+  componentDidMount() {
+    fetch("http://localhost:4000", {
+      method: "GET",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        query: `
           query {
             todos {
               edges {
@@ -21,28 +22,28 @@ class Category extends Component {
                 }
           }
             }
-          }` 
-        }),
-      })
-      .then(res => res.json())
-      .then(res => console.log(res.data));
-  } 
+          }`,
+      }),
+    })
+      .then((res) => res.json())
+      .then((res) => console.log(res.data));
+  }
 
   render() {
-      return(
-          <div className="category">
-              <Navbar/>
-              <h2 className="header">Women Category</h2>
-              <div className="product-container">
-                  <Product photo={wool} productName='wool hood' productPrice='$50.00'/>
-                  <Product photo={wool} productName='wool hood' productPrice='$50.00'/>
-                  <Product photo={wool} productName='wool hood' productPrice='$50.00'/>
-                  <Product photo={wool} productName='wool hood' productPrice='$50.00'/>
-                  <Product photo={wool} productName='wool hood' productPrice='$50.00'/>
-                  <Product photo={wool} productName='wool hood' productPrice='$50.00'/>
-              </div>
-          </div>
-      );
+    return (
+      <div className="category">
+        <Navbar />
+        <h2 className="header">Women Category</h2>
+        <div className="product-container">
+          <Product photo={wool} productName="wool hood" productPrice="$50.00" />
+          <Product photo={wool} productName="wool hood" productPrice="$50.00" />
+          <Product photo={wool} productName="wool hood" productPrice="$50.00" />
+          <Product photo={wool} productName="wool hood" productPrice="$50.00" />
+          <Product photo={wool} productName="wool hood" productPrice="$50.00" />
+          <Product photo={wool} productName="wool hood" productPrice="$50.00" />
+        </div>
+      </div>
+    );
   }
 }
 
