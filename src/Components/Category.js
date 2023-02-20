82524a5db715ec771ddfe5a464d2f4f02d87db58
bentike <ajadiyusuf73@gmail.com>
2023-02-20 22:23:34 +0100

    working on graphql query

--- src/Components/Category.js
+++ src/Components/Category.js
@@ -1,38 +1,49 @@
-import React, {Component} from "react";
-import { useQuery, gql } from '@apollo/client';
+import React, { Component } from "react";
+import { useQuery, gql } from "@apollo/client";
 import Product from "./Product";
 import Navbar from "./Navbar";
-import '../Styles/Category.css';
-import wool from '../Images/wool.png';
+import "../Styles/Category.css";
+import wool from "../Images/wool.png";
 
-const GET_PRODUCTS = gql`
-  query GetLocations {
-    locations {
-      id
-      name
-      description
-      photo
-    }
+class Category extends Component {
+  componentDidMount(){
+    fetch('http://localhost:4000', {
+        method: 'GET',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ query: `
+          query {
+            todos {
+              edges {
+                node {
+                  completed
+                  id
+                  text
+                }
+          }
+            }
+          }` 
+        }),
+      })
+      .then(res => res.json())
+      .then(res => console.log(res.data));
+  } 
+
+  render() {
+      return(
+          <div className="category">
+              <Navbar/>
+              <h2 className="header">Women Category</h2>
+              <div className="product-container">
+                  <Product photo={wool} productName='wool hood' productPrice='$50.00'/>
+                  <Product photo={wool} productName='wool hood' productPrice='$50.00'/>
+                  <Product photo={wool} productName='wool hood' productPrice='$50.00'/>
+                  <Product photo={wool} productName='wool hood' productPrice='$50.00'/>
+                  <Product photo={wool} productName='wool hood' productPrice='$50.00'/>
+                  <Product photo={wool} productName='wool hood' productPrice='$50.00'/>
+              </div>
+          </div>
+      );
   }
-`;
-
-class Category extends Component{
-    render(){
-        return(
-            <div className="category">
-                <Navbar/>
-                <h2 className="header">Women Category</h2>
-                <div className="product-container">
-                    <Product photo={wool} productName='wool hood' productPrice='$50.00'/>
-                    <Product photo={wool} productName='wool hood' productPrice='$50.00'/>
-                    <Product photo={wool} productName='wool hood' productPrice='$50.00'/>
-                    <Product photo={wool} productName='wool hood' productPrice='$50.00'/>
-                    <Product photo={wool} productName='wool hood' productPrice='$50.00'/>
-                    <Product photo={wool} productName='wool hood' productPrice='$50.00'/>
-                </div>
-            </div>
-        );
-    }
 }
 
 export default Category;
