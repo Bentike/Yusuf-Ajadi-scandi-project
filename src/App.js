8eaa85b544cddd46305b6b03caebd0b35af0a235
bentike <ajadiyusuf73@gmail.com>
2023-02-21 12:09:13 +0100

    changed App.js to a class component

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React, {Component} from 'react';
 import Category from './Components/Category';
 import {Routes, Route} from 'react-router-dom';
 import ProductDescription from './Components/ProductDescription';
@@ -6,17 +7,19 @@ import Kids from './Components/Kids';
 import Men from './Components/Men';
 import './App.css';
 
-function App() {
-  return (
-    <Routes>
-        <Route path='/' element={<Category />}/>
-        <Route path='women' element={<Category />}/>
-        <Route path='men' element={<Men />}/>
-        <Route path='kids' element={<Kids />}/>
-        <Route path='product' element={<ProductDescription />}/>
-        <Route path='cart' element={<CartPage />}/>
-    </Routes>
-  );
+class App extends Component {
+  render() {
+    return (
+      <Routes>
+          <Route path='/' element={<Category />}/>
+          <Route path='women' element={<Category />}/>
+          <Route path='men' element={<Men />}/>
+          <Route path='kids' element={<Kids />}/>
+          <Route path='product' element={<ProductDescription />}/>
+          <Route path='cart' element={<CartPage />}/>
+      </Routes>
+    );
+  } 
 }
 
 export default App;
