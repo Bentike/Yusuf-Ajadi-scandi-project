e9c6792fc0238c2340004d108def42c5366b2db0
bentike <ajadiyusuf73@gmail.com>
2023-02-28 12:16:46 +0100

    extracted Sizes component from SizeAndColor component

---
+++ src/Components/Sizes.js
@@ -0,0 +1,19 @@
+import React, {Component} from "react";
+
+class Sizes extends Component{
+    render(){
+        return(
+            <div>
+                <p>Size:</p>
+                <div className="sizes">
+                    <p>XS</p>
+                    <p>S</p>
+                    <p>M</p>
+                    <p>L</p>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Sizes;
