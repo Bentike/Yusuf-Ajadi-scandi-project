08a6d194bd96edecb8cb3db50d658e86d969c172
bentike <ajadiyusuf73@gmail.com>
2023-02-28 12:18:58 +0100

    extracted Colors component from SizeAndColor component

---
+++ src/Components/Colors.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+
+class Colors extends Component {
+  render() {
+    return (
+      <div>
+        <p>Color:</p>
+        <div className="colors">
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Colors;
