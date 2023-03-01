c7aa8b12ff1fb09b9f14df1a9f0fa24711602573
bentike <ajadiyusuf73@gmail.com>
2023-03-01 12:25:04 +0100

    formatted Sizes.js

--- src/Components/Sizes.js
+++ src/Components/Sizes.js
@@ -1,27 +1,29 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 
-class Sizes extends Component{
-    constructor(props){
-        super(props);
-        this.sizeRef = React.createRef()
-    }
+class Sizes extends Component {
+  constructor(props) {
+    super(props);
+    this.sizeRef = React.createRef();
+  }
 
-    render(){
-        const getSize = () => {
-            const mySize = document.getElementsByClassName("my-size");
-            for(let size of mySize){
-                size.classList.remove("selected-color");
-            }
-            this.sizeRef.current.classList.toggle("selected-color");
-        }
-        return(
-            <div>
-                <div className="sizes">
-                    <p className="my-size" onClick={getSize} ref={this.sizeRef}>{this.props.size}</p>
-                </div>
-            </div>
-        )
-    }
+  render() {
+    const getSize = () => {
+      const mySize = document.getElementsByClassName("my-size");
+      for (let size of mySize) {
+        size.classList.remove("selected-color");
+      }
+      this.sizeRef.current.classList.toggle("selected-color");
+    };
+    return (
+      <div>
+        <div className="sizes">
+          <p className="my-size" onClick={getSize} ref={this.sizeRef}>
+            {this.props.size}
+          </p>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Sizes;
