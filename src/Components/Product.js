9f6a8315e33c2c190b3e8dfd62fe189e63ae7b3a
bentike <ajadiyusuf73@gmail.com>
2023-03-03 12:40:25 +0100

    formatted Product.js

--- src/Components/Product.js
+++ src/Components/Product.js
@@ -1,37 +1,41 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import { NavLink } from "react-router-dom";
-import cart from '../Images/cart.png';
-import '../Styles/Product.css';
+import cart from "../Images/cart.png";
+import "../Styles/Product.css";
 
-class Product extends Component{
-    constructor(props){
-       super(props);
-       this.cartRef = React.createRef();
-    }
+class Product extends Component {
+  constructor(props) {
+    super(props);
+    this.cartRef = React.createRef();
+  }
 
-    render(){
+  render() {
+    const handleMouseEnter = () => {
+      this.cartRef.current.style.display = "block";
+    };
 
-        const handleMouseEnter = () => {
-            this.cartRef.current.style.display = "block";
-        }
+    const handleMouseLeave = () => {
+      this.cartRef.current.style.display = "none";
+    };
 
-        const handleMouseLeave = () => {
-            this.cartRef.current.style.display = "none";
-        }
+    const { photo, productName, productPrice } = this.props;
 
-        const {photo, productName, productPrice} = this.props;
-
-        return(
-            <div className="product-wrap" onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave} onClick={() => this.props.handleProductClick(productName)}>
-                <NavLink className="prod" to="/product">
-                 <img className="product-img" src={photo} alt="product"/>
-                 <p>{productName}</p>
-                 <p>{productPrice}</p>
-                 <img id="cart" src={cart} alt="add to cart" ref={this.cartRef}/>
-                </NavLink> 
-            </div>
-        );
-    }
+    return (
+      <div
+        className="product-wrap"
+        onMouseEnter={handleMouseEnter}
+        onMouseLeave={handleMouseLeave}
+        onClick={() => this.props.handleProductClick(productName)}
+      >
+        <NavLink className="prod" to="/product">
+          <img className="product-img" src={photo} alt="product" />
+          <p>{productName}</p>
+          <p>{productPrice}</p>
+          <img id="cart" src={cart} alt="add to cart" ref={this.cartRef} />
+        </NavLink>
+      </div>
+    );
+  }
 }
 
 export default Product;
