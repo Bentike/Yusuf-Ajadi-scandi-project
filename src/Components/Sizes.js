4bd558b8915db48431e5090b477310e83c17414c
bentike <ajadiyusuf73@gmail.com>
2023-03-07 13:52:11 +0100

    set the selected size attribute on clicking a size

--- src/Components/Sizes.js
+++ src/Components/Sizes.js
@@ -8,18 +8,20 @@ class Sizes extends Component {
 
   render() {
     // This method gets The size attribute of a selected product;
+    // It also runs the props.setSize method to set the selected size attribute;
     const getSize = () => {
       const mySize = document.getElementsByClassName("my-size");
       for (let size of mySize) {
         size.classList.remove("selected-size");
       }
       this.sizeRef.current.classList.toggle("selected-size");
+      this.props.setSize(this.sizeRef.current.textContent);
     };
 
     return (
       <div>
         <div className="sizes">
-          <p className="my-size" onClick={() => this.props.setSize("Hello")} ref={this.sizeRef}>
+          <p className="my-size" onClick={getSize} ref={this.sizeRef}>
             {this.props.size}
           </p>
         </div>
