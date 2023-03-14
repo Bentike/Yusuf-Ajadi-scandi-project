5e8b9b19d7053f2ddc270a7bb4e3a287f079fb9b
bentike <ajadiyusuf73@gmail.com>
2023-03-14 12:06:16 +0100

    added uique keys

--- src/Components/CartItem.js
+++ src/Components/CartItem.js
@@ -1,39 +1,66 @@
-import React, {Component} from "react";
-import '../Styles/CartItem.css';
+import React, { Component } from "react";
+import uniqid from "uniqid";
+import "../Styles/CartItem.css";
 import Colors from "./Colors";
 import Sizes from "./Sizes";
 
 class CartItem extends Component {
-    render(){
-        return(
-            <div className="cart">
-                <div className="cart-item">
-                    <div>
-                        <h3>{this.props.name}</h3>
-                        <h4>{this.props.brand}</h4>
-                        <p>{this.props.symbol}{this.props.price}</p>
-                        {/* to verify if index work for keys Insha Allah */}
-                        {this.props.sizes ? this.props.sizes.map((item, idx) => {
-                           return <Sizes key={idx} size={item.value} setSize={this.props.setSize}/>
-                        }) : this.props.colors ? this.props.colors.map((color, idx) => {
-                            return <Colors key={idx} color={color.value} setColor={this.props.setColor}/> 
-                        }) : ""}
-                    </div>
-                    <div className="item-image-wrap">
-                        <div className="item-image">
-                            <button className="cart-btn"><span className="text">+</span></button>
-                            <button className="quantity"><span className="text">{this.props.quantity}</span></button>
-                            <button className="cart-btn"><span className="text">-</span></button>
-                        </div>
-                        <div>
-                            <img className="product-display" src={this.props.image} alt="wool"/>
-                        </div>
-                    </div>
-                </div>
-                <hr/>
-           </div>
-        );
-    }
+  render() {
+    return (
+      <div className="cart">
+        <div className="cart-item">
+          <div>
+            <h3>{this.props.name}</h3>
+            <h4>{this.props.brand}</h4>
+            <p>
+              {this.props.symbol}
+              {this.props.price}
+            </p>
+            {this.props.sizes
+              ? this.props.sizes.map((item) => {
+                  return (
+                    <Sizes
+                      key={uniqid()}
+                      size={item.value}
+                    />
+                  );
+                })
+              : this.props.colors
+              ? this.props.colors.map((color) => {
+                  return (
+                    <Colors
+                      key={uniqid()}
+                      color={color.value}
+                    />
+                  );
+                })
+              : ""}
+          </div>
+          <div className="item-image-wrap">
+            <div className="item-image">
+              <button className="cart-btn">
+                <span className="text">+</span>
+              </button>
+              <button className="quantity">
+                <span className="text">{this.props.quantity}</span>
+              </button>
+              <button className="cart-btn">
+                <span className="text">-</span>
+              </button>
+            </div>
+            <div>
+              <img
+                className="product-display"
+                src={this.props.image}
+                alt="wool"
+              />
+            </div>
+          </div>
+        </div>
+        <hr />
+      </div>
+    );
+  }
 }
 
 export default CartItem;
