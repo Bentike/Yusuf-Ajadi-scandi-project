eb91de32349f614ccb98796ec550a6476ea51eec
bentike <ajadiyusuf73@gmail.com>
2023-05-19 11:58:15 +0100

    extracted OverlayCartItems component

--- src/Components/OverlayCart.js
+++ src/Components/OverlayCart.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import "../Styles/OverlayCart.css";
 import MiniCheckout from "./MiniCheckout";
-import photo from "../Images/wool.png"
+import OverlayCartItems from "./OverlayCartItems";
 
 class OverlayCart extends Component {
   render() {
@@ -10,35 +10,7 @@ class OverlayCart extends Component {
         <h5 className="myBag">
           My Bag,<span className="overlayCartItem"> 3 items</span>
         </h5>
-        <div className="overlayCartWrap">
-          <div>
-            <p className="cartOverlayItemName">Apollo Running Short</p>
-            <p className="cartOverlayPrice">$50.00</p>
-            <p className="size">size</p>
-            <div className="sizes">
-                <div className="sizeBox">XS</div>
-                <div className="sizeBox">S</div>
-                <div className="sizeBox">M</div>
-                <div className="sizeBox">L</div>
-            </div>
-            <p className="cartColor">color</p>
-            <div className="cartColors">
-                <div className="overlayColors"></div>
-                <div className="overlayColors"></div>
-                <div className="overlayColors"></div>
-            </div>
-          </div>
-          <div className="controlWrap">
-              <div className="cartBtnDiv">
-                 <button className="overlayCartBtn">+</button>
-                 <p className="cartQuantity">1</p>
-                 <button className="overlayCartBtn">-</button>
-              </div>
-              <div>
-                <img src={photo} alt="item" className="overlayCartImg"/>
-              </div>
-          </div>
-        </div>
+        <OverlayCartItems/>
         <MiniCheckout/>
       </div>
     );
---
+++ src/Components/OverlayCartItems.js
@@ -0,0 +1,41 @@
+import React, { Component } from "react";
+import photo from "../Images/wool.png"
+
+
+class OverlayCartItems extends Component {
+  render() {
+    return (
+      <div className="overlayCartWrap">
+        <div>
+          <p className="cartOverlayItemName">Apollo Running Short</p>
+          <p className="cartOverlayPrice">$50.00</p>
+          <p className="size">size</p>
+          <div className="sizes">
+            <div className="sizeBox">XS</div>
+            <div className="sizeBox">S</div>
+            <div className="sizeBox">M</div>
+            <div className="sizeBox">L</div>
+          </div>
+          <p className="cartColor">color</p>
+          <div className="cartColors">
+            <div className="overlayColors"></div>
+            <div className="overlayColors"></div>
+            <div className="overlayColors"></div>
+          </div>
+        </div>
+        <div className="controlWrap">
+          <div className="cartBtnDiv">
+            <button className="overlayCartBtn">+</button>
+            <p className="cartQuantity">1</p>
+            <button className="overlayCartBtn">-</button>
+          </div>
+          <div>
+            <img src={photo} alt="item" className="overlayCartImg" />
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default OverlayCartItems;
