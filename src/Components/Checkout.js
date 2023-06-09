b5574e12780fda3a8baaf2fb5ffe391873c8aea1
bentike <ajadiyusuf73@gmail.com>
2023-06-09 10:52:45 +0100

    removed tax from checkout

--- src/Components/Checkout.js
+++ src/Components/Checkout.js
@@ -5,7 +5,6 @@ class Checkout extends Component{
     render(){
         return(
             <div className="checkout">
-                <p>Tax 21% : <span className="val">$40</span></p>
                 <p>Quantity : <span className="val qty">{this.props.quantity}</span></p>
                 <p>Total : <span className="val total">{this.props.total}</span></p>
                 <button className="checkout-btn" onClick={this.props.checkout}>ORDER</button>
